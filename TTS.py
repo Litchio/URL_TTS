66ed2832b8fbe2b741f31243e6c56449b4209adf
Litchio <69739087+Litchio@users.noreply.github.com>
2021-06-25 00:01:11 +0200

    Add files via upload

--- TTS.py
+++ TTS.py
@@ -9,7 +9,12 @@ from bs4 import BeautifulSoup
 
 pygame.mixer.init()
 engine = pyttsx3.init()
-
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
 def TextOfHtml(file):
     soup = BeautifulSoup(file, 'html.parser')
@@ -29,7 +34,9 @@ def ReadText(text):
 
 
 def FileFromURL(url):
-    file = urllib.request.urlopen(url).read()
+    request = urllib.request.Request(url, None, headers=hdr)
+    response = urllib.request.urlopen(request)
+    file = response.read()
     return file
 
 
